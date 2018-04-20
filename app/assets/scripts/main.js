67c62baf67cbce83efce37c8500fc0a8d7566f65
Arun Kumar <Arun.Kumar5@sc.com>
2018-04-20 21:19:48 +0530

    Updating the booking widget

--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -173,6 +173,20 @@ const FE = {
             feed.run();
         },
 
+        showBookingTab: (evt, tabName) => {
+            var i, tabcontent, tablinks;
+            tabcontent = document.getElementsByClassName("tabcontent");
+            for (i = 0; i < tabcontent.length; i++) {
+                tabcontent[i].style.display = "none";
+            }
+            tablinks = document.getElementsByClassName("tablinks");
+            for (i = 0; i < tablinks.length; i++) {
+                tablinks[i].className = tablinks[i].className.replace(" active", "");
+            }
+            document.getElementById(tabName).style.display = "block";
+            evt.currentTarget.className += " active";
+        },
+
         googleMap: () => {
             let selectorMapElement = document.getElementById('gmap_canvas');
             if (typeof(selectorMapElement) != 'undefined' && selectorMapElement != null) {
@@ -222,7 +236,6 @@ const FE = {
 
 $(function() {
     FE.global.init();
-
 });
 
 $(window).load(function() {
