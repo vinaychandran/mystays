7afcb2386dec13a27f0c88bc3bf0d351e3379588
Thirumalesh <thirumalesh.berbalaje@sc.com>
2018-04-26 16:19:11 +0530

    Add adult children room addition

--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -289,9 +289,19 @@ const FE = {
         $(this).parents('.input-showtext').find('.popup-menu').fadeOut();
         $(this).parents('.input-showtext').removeClass('focus');
       });
+      $(document).on('click', '.people-list-popup .btn-group .done', function(e) {
+        e.preventDefault();
+        var popup = $(this).parents('.people-list-popup')
+        let getText = '大人'+ popup.find('.grown-up .input-showtext input').val() + ' 名, 子供' + popup.find('.children .input-showtext input').val() + ' 名 <span>' + popup.find('.room .input-showtext input').val() + ' 部屋 </span>';
+        $('.people .people-list p').html(getText);
+        $(this).closest('body').children('.popup-wrap').css('display', 'none');
+      });
       $(document).on('click', '.people-list-popup .btn-group .clear', function(e) {
         e.preventDefault();
-        $(this).closest('body').children('.popup-wrap').css('display', 'none');
+        var popup = $(this).parents('.people-list-popup')
+        popup.find('.grown-up .input-showtext input').val('');
+        popup.find('.children .input-showtext input').val('');
+        popup.find('.room .input-showtext input').val('');
       });
     },
     itemShowHide: () => {
