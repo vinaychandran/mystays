2dfecc2d60455e0a8ef52ce487360bd5610df9a9
Vinay Chandra <vinaychandra58@gmail.com>
2018-05-22 16:32:05 +0900

    Venue page update

--- app/assets/narita/scripts/main.js
+++ app/assets/narita/scripts/main.js
@@ -493,6 +493,78 @@ const FE = {
                 }, false);
             })
         },
+        filter: (targetElement) => {
+            // get all of our list items
+            let itemsToFilter = document.querySelectorAll(".itemsToFilter li");
+              
+            //setup click event handlers on our checkboxes
+            let checkBoxes = document.querySelectorAll(".filterSection li input");
+              
+            for (let i = 0; i < checkBoxes.length; i++) {
+                checkBoxes[i].addEventListener("click", filterItems, false);
+                //checkBoxes[i].checked = true;
+            }
+              
+            // the event handler!
+            function filterItems(e) {
+                var clickedItem = e.target;
+                  
+                if (clickedItem.checked == true) {
+                    hideOrShowItems(clickedItem.value, "hideItem", "showItem");
+                } else if (clickedItem.checked == false) {
+                    hideOrShowItems(clickedItem.value, "showItem", "hideItem");
+                } else {
+                    // deal with the indeterminate state if needed
+                }
+            }
+              
+            // add or remove classes to show or hide our content
+            function hideOrShowItems(itemType, classToRemove, classToAdd) {
+                for (var i = 0; i < itemsToFilter.length; i++) {
+                    var currentItem = itemsToFilter[i];
+                      
+                    if (currentItem.getAttribute("data-type") == itemType) {
+                        removeClass(currentItem, classToRemove);
+                        addClass(currentItem, classToAdd);
+                    }
+                }
+            }
+              
+            //
+            // Helper functions for adding and removing class values
+            //
+            function addClass(element, classToAdd) {
+                var currentClassValue = element.className;
+                    
+                if (currentClassValue.indexOf(classToAdd) == -1) {
+                    if ((currentClassValue == null) || (currentClassValue === "")) {
+                        element.className = classToAdd;
+                    } else {
+                        element.className += " " + classToAdd;
+                    }
+                }
+            }
+                    
+            function removeClass(element, classToRemove) {
+                var currentClassValue = element.className;
+              
+                if (currentClassValue == classToRemove) {
+                    element.className = "";
+                    return;
+                }
+              
+                var classValues = currentClassValue.split(" ");
+                var filteredList = [];
+              
+                for (var i = 0 ; i < classValues.length; i++) {
+                    if (classToRemove != classValues[i]) {
+                        filteredList.push(classValues[i]);
+                    }
+                }
+              
+                element.className = filteredList.join(" ");
+            }
+        },
 
         showCheckBoxAction: () => {
             $(document).on('click', '.form-checkbox .checkbox-style input', function() {
@@ -539,7 +611,7 @@ const FE = {
             FE.global.autocomplatePopup();
             FE.global.itemShowHide();
             FE.global.filterRooms('room-types');
-            FE.global.filterRooms('venue-types');
+            FE.global.filter('venue-types');
             FE.global.datePickerInit('.date-picker-tab1', 'ja', false);
             FE.global.datePickerInit('.date-picker-tab2-single', 'ja', true);
             FE.global.datePickerInit('.date-picker-tab3', 'ja', false);
